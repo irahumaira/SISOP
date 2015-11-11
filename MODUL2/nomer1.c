654e9733716c3340353ca89a8be249a0388cbb55
irahumaira <poenya.ira@gmail.com>
2015-11-11 14:50:56 +0700

    upload nomer 1 mini shell

---
+++ MODUL2/nomer1.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+void  parse(char *line, char **argv)
+{
+	while (*line != '\0')
+	{
+		while (*line == ' ' || *line == '\t' || *line == '\n')
+		*line++ = '\0';
+		*argv++ = line;
+		while (*line != '\0' && *line != ' ' && *line != '\t' && *line != '\n')
+		line++;
+	}
+	*argv = '\0';
+}
+
+int  exec(char **argv, int back) 
+{
+     	pid_t  pid;
+     	int status;
+     	if ((pid = fork()) < 0) 
+	{
+		printf("*** ERROR: forking child process failed\n");
+		exit(1);
+     	}
+   	else if (pid == 0) 
+	{
+		if (execvp(*argv, argv) < 0)
+		{
+              	 	printf("*** ERROR: exec failed\n");
+              	 	exit(1);
+		}
+    	}
+     	else
+	{
+         	if(back==0) while (wait(&status) != pid);
+	}
+}
+
+void sighandler(int signum)
+{
+	char tmp[30];
+	getcwd(tmp, sizeof(tmp));
+	printf("\npraktikum2:~%s ", tmp);
+//	fflush(stdout);
+}
+
+int main()
+{
+	signal(SIGINT, sighandler);
+	signal(SIGTSTP, sighandler);
+	
+	char line[1024],*argv[64];
+	char tmp[30],ori[30];
+	while(1) 
+	{
+		getcwd(tmp, sizeof(tmp));
+	        printf("praktikum2:~ %s ", tmp);
+		scanf("%[^\n]",line);
+		if(getchar()==EOF) exit(1);
+		printf(">> %s\n",line);
+		
+		if(strcmp(line,"exit")!=0) 
+		{
+			strcpy(ori,line);
+			char *inp[5],*split=strtok(line," ");
+			int i=-1;
+			while(split!=NULL)
+			{
+			        inp[++i]=split;
+				printf("hasil strtok(): %s\n",inp[i]);
+				split=strtok(NULL," ");
+			}
+			if(strcmp(inp[0],"cd")!=0) 
+			{			
+				parse(ori,argv);
+				exec(argv,0);
+			}
+		}
+		else return;
+		strcpy(line,"");
+	}
+}
+
