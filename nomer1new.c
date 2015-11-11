374cf518910f7bb0219222583d8e9c2746eaff06
faizainun <faiz.karima@gmail.com>
2015-11-11 13:51:00 +0700

    salah upload .... nomer1 new + background process

--- nomer1new.c
+++ nomer1new.c
@@ -1,60 +1,83 @@
-#include<stdio.h>
-#include<signal.h>
-#include<string.h>
-
-typedef struct nama
-{
-	char nama1[30],nama2[30],nama3[30];
-}	nfile;
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
 
-void *thread1(void *args) {
-	FILE *inp,*out;
-	char cobi[100];
-	nfile coba = *((nfile *)args);
-
-	inp = fopen(coba.nama1,"r");
-	out = fopen(coba.nama2,"rw");
-	while(fgets(cobi,100,inp)){
-            fprintf(out,"%s",cobi);
+void  parse(char *line, char **argv) {
+	while (*line != '\0') {       // if not the end of line .......  
+		while (*line == ' ' || *line == '\t' || *line == '\n')	*line++ = '\0';
+          	*argv++ = line;
+		while (*line != '\0' && *line != ' ' &&  *line != '\t' && *line != '\n') line++;
 	}
+	*argv = '\0';                 // mark the end of argument list
 }
 
-void *thread2(void *args) {
-	FILE *inp2,*out2;
-        char cobi[100];
-        nfile coba2 = *((nfile *)args);
-
-        inp2 = fopen(coba2.nama2,"rw");
-        out2 = fopen(coba2.nama3,"w");
-        while(fgets(cobi,100,inp2)){
-            fprintf(out2,"%s",cobi);
-        }
-}
-
-void main() {
-	nfile inm;
-	FILE *file1;
-	while(1) {
-		printf("nama file1 : ");
-		scanf("%s",inm.nama1);
-		file1 = fopen(inm.nama1, "r");
-		if(file1==NULL)
-			printf("file tidak ditemukan !!\n");
-		else {
-			printf("nama file2 : ");
-			scanf("%s",inm.nama2);
-			printf("nama file3 : ");
-                        scanf("%s",inm.nama3);
-			break;
+int  exec(char **argv, int back) {
+     	pid_t  pid;
+     	int status;
+     	if ((pid = fork()) < 0) {
+		printf("*** ERROR: forking child process failed\n");
+		exit(1);
+     	}
+   	else if (pid == 0) {
+		if (execvp(*argv, argv) < 0) {
+              	 	printf("*** ERROR: exec failed\n");
+              	 	exit(1);
 		}
+    	}
+     	else {
+         	if(back==0) while (wait(&status) != pid);
 	}
+}
 
-	pthread_t t1,t2;
-	pthread_create(&t1,NULL,thread1,&inm);
-//	pthread_join(t1,NULL);
+void sighandler(int signum) {
+	char tmp[30];
+	getcwd(tmp, sizeof(tmp));
+	printf("\npraktikum2:~%s ", tmp);
+	fflush(stdout);
+}
 
-	pthread_create(&t2,NULL,thread2,&inm);
-//	pthread_join(t1,NULL);
-	pthread_join(t2,NULL);
+
+int main() {
+	signal(SIGINT, sighandler);
+	signal(SIGTSTP, sighandler);
+	
+	char line[1024],*argv[64];
+	char tmp[30],ori[30];
+	while(1) 
+	{
+		getcwd(tmp, sizeof(tmp));
+	        printf("praktikum2:~ %s$ ", tmp);
+		scanf("%[^\n]",line);
+		if(getchar()==EOF) exit(1);
+		printf(">> %s\n",line);
+		
+		if(strcmp(line,"exit")!=0) {
+			if(strncmp("cd",line,2)!=0) {
+				char *inp,*split=strtok(line,"&");
+				int count=0;
+				while(split!=NULL) {
+count++;
+				        inp=split;
+					printf("%s\n",inp);
+					split=strtok(NULL,"&");
+				}
+				printf("%d\n",count);
+				parse(line,argv);
+				exec(argv,0);
+			}
+			else {
+				char *inp,*split=strtok(line," ");
+				while(split!=NULL) {
+				        inp=split;
+					split=strtok(NULL," ");
+				}
+				chdir(inp);
+			}
+		}
+		else exit(1);
+		strcpy(line,"");
+	}
 }
